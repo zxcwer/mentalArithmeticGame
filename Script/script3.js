e4a9d74f9bd537a7bad643e28ae35efca6d57433
zxcwer <keekeoduangdy@gmail.com>
2022-11-29 10:53:02 +0900

    'updated level1-5'

---
+++ Script/script3.js
@@ -0,0 +1,65 @@
+let questionNumber = 0; //問題の番目
+let yes = document.getElementById("yes"); //"正"のボタン情報取得
+let no = document.getElementById("no"); //"誤"のボタン情報取得
+let correcting = "F";
+let right = 0;
+let wrong = 0;
+
+//data
+function update(i) {
+  fetch("./Question/level3.json")
+    .then((response) => response.json())
+    .then((data) => {
+      let no1 = data[i].text[0];
+      let symbol = data[i].text[1];
+      let no2 = data[i].text[2];
+      let no3 = data[i].text[3];
+      correcting = data[i].answer;
+      document.querySelector("#box").innerHTML = `
+          <div class="item"> ${no1}</div>
+          <div class="item"> ${symbol}</div>
+          <div class="item"> ${no2}</div>
+          <div class="item"> = </div>
+          <div class="item"> ${no3}</div>
+          `;
+    });
+}
+
+//countdown
+let i = 0;
+function countdown() {
+  let timeleft = 5;
+  var downloadTimer = setInterval(function () {
+    document.getElementById("seconds").textContent = timeleft - i;
+    if (timeleft - i <= 0) {
+      i = 0;
+      exit();
+      questionNumber++;
+      update(questionNumber);
+    } else {
+      i++;
+    }
+  }, 800);
+}
+
+function button(checkId) {
+  i = 0;
+  questionNumber++;
+  exit();
+  update(questionNumber);
+
+  if (checkId == correcting) {
+    right++;
+    document.getElementById("right").textContent = right;
+  } else if (checkId != correcting) {
+    wrong++;
+    document.getElementById("wrong").textContent = wrong;
+  }
+}
+
+function exit() {
+  if (questionNumber == 20) {
+    alert(`正：${right} 誤：${wrong} 正解率：${(right * 100) / 20}%`);
+  }
+}
+countdown();
